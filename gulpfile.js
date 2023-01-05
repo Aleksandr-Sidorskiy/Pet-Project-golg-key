0dd65172bda680496050800e99b32511d7403be9
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2023-01-05 21:28:10 +0200

    setting gulp folder dist

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+let project_folder = "dist";
+let source_folder = "#src";
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css",
+    js: project_folder + "/js",
+    img: project_folder + "/img",
+    fonts: project_folder + "/fonts",
+  },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/app.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,ico,webp}",
+    fonts: source_folder + "/fonts/*.ttf",
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,ico,webp}",
+  },
+  clean: "./" + project_folder + "/",
+};
+
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  browserSync = require("browser-sync").create();
+fileinclude = require("gulp-file-include");
+
+function browserSyncFun() {
+  browserSync.init({
+    server: {
+      baseDir: "./" + project_folder + "/",
+    },
+    port: 3000,
+    notify: false,
+  });
+}
+
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.html))
+    .pipe(browserSync.stream());
+}
+
+function watchFile() {
+  gulp.watch([path.watch.html], html);
+}
+
+// function css() {
+//   return src(path.src.css)
+//     .pipe(dest(path.build.css))
+//     .pipe(browserSync.stream());
+// }
+
+let build = gulp.series(html);
+let watch = gulp.parallel(build, watchFile, browserSyncFun);
+
+exports.html = html;
+// exports.css = css;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
