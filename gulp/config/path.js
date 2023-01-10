11253cde68de4aedae015565b1a3f0db1b8dbf05
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2023-01-10 21:04:57 +0200

    no working gulp-file-include

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,17 +6,20 @@ const srcFolder = "./src";
 
 export const path = {
   build: {
+    html: `${buildFolder}/`,
     files: `${buildFolder}/files/`,
   },
   src: {
+    html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
   watch: {
+    html: `${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
   clean: buildFolder,
   buildFolder: buildFolder,
   srcFolder: srcFolder,
   rootFolder: rootFolder,
-  ftp: "",
+  ftp: ``,
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ import { path } from "./gulp/config/path.js";
 // імпорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+
 // Передаємо зеачення в глобальну змінну
 global.app = {
   path: path,
@@ -12,10 +14,14 @@ global.app = {
 // наглядаємо за змінами у файлах
 function watcher() {
   gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
 }
 
+const mainTasks = gulp.parallel(html, copy);
+
 // створення сценарію виконання задач
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
+
 // виконуєм сценарій за замовчуванням
 gulp.task("default", dev);
 
