60c47e7632944f1e2a7376a5de1be78fa0653d8b
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2023-01-11 21:20:19 +0200

    gulp

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,11 +1,10 @@
 import fileInclude from "gulp-file-include";
-import bs from "browser-sync";
-// const fileinclude = require("gulp-file-include");
+import src from "gulp";
+// import bs from "browser-sync";
 
 export const html = () => {
   return app.gulp
     .src(app.path.src.html)
     .pipe(fileInclude())
-    .pipe(app.gulp.dest(app.path.build.html))
-    .pipe(bs.stream());
+    .pipe(app.gulp.dest(app.path.build.html));
 };
